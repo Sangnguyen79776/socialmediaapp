c10fdd1480574a6c916dd808d8228979c32e129e
Sangnguyen79776 <145439978+Sangnguyen79776@users.noreply.github.com>
2023-12-23 13:04:24 -0600

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <html>
 	<head>
-		<title>TwitterClone</title>
+		<title>Social Media App</title>
 		<meta charset="UTF-8" />
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css"/>
         <!-- <link rel="stylesheet" href="assets/css/style-complete.css"/> -->
